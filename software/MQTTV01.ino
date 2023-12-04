330e14cfbc20cc16738e3756e54e67d7605b4800
Víctor Mengual <100873122+vmpuma@users.noreply.github.com>
2023-12-04 10:46:45 +0100

    Update MQTTV01.ino

--- software/MQTTV01.ino
+++ software/MQTTV01.ino
@@ -15,6 +15,16 @@
 #include <ESP8266WiFi.h>
 #endif
 
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#define seaLevelPressure_hPa 1013.25
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+
+Adafruit_MPU6050 mpu;
+Adafruit_BMP085 bmp;
+
 
 //Mqtt Client
 WiFiClient wClient;
@@ -34,6 +44,9 @@ String ID_PLACA;
 String conexion; // for LastWill & Testament
 String datos_uv; // publish topic
 String data; //subscribe topic
+
+// Pin numbers
+const int uv_pin = 23;
 
 //-----------------------------------------------------
 //Set Up WiFi Connection
@@ -81,8 +94,20 @@ void setup() {
    Serial.begin(115200);
   Serial.println();
   Serial.println("Empieza setup...");
-   // ID de la placa
-  ID_PLACA = "ESP_" + String( ESP.getChipId() );
+  
+  // ID de la placa
+  #ifdef ESP32
+    uint32_t id = 0;
+    for(int i = 0; i < 17; i = i+8) {
+      id |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+    }
+    ID_PLACA = "ESP_" + String(id);
+  #endif
+  // chiID specific for ESP8266
+  #ifdef ESP8266
+    ID_PLACA = "ESP_" + String(ESP.getChipId());
+  #endif
+
    // Topics
   conexion = "infind/GRUPO15/conexion";
   datos_uv = "infind/GRUPO15/uv";
@@ -107,6 +132,80 @@ void setup() {
  
  //Publicamos el mensaje
   mqtt_client.publish(conexion.c_str(), mensaje.c_str(),true);
+
+  if (!bmp.begin()) {
+  Serial.println("BMP180 Not Found. CHECK CIRCUIT!");
+  while (1) {}
+  }
+
+  Serial.println("Adafruit MPU6050 test!");
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
 
   //Mensajes de finalización de setup 
   Serial.println("Identificador placa: "+ ID_PLACA);
@@ -135,14 +234,14 @@ void loop() {
 
   unsigned long ahora = millis();
     
-  if (ahora - ultimo_mensaje >= 30000) {
+  if (ahora - ultimo_mensaje >= 5000) {
     ultimo_mensaje = ahora;
 
     // Lectura del sensor  
-    float dato_uv = serialRead(A0);
+    float dato_uv = analogRead(uv_pin);
     
     //Mensaje formateado en JSON con los datos de los sensores y de conexión Wifi 
-    String mensaje = "{\"RadiacionDHT11\":"+ String(dato_uv) +"}";
+    String mensaje = "{\"Radiacion\":"+ String(dato_uv) +"}";
    
     //Imprimimos el mensaje en el monitor serie
     Serial.println();
@@ -152,6 +251,57 @@ void loop() {
     //Publicamos el mensaje
     mqtt_client.publish(datos_uv.c_str(), mensaje.c_str());
   
+  }
+
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+    
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Pressure at sealevel (calculated) = ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+
+  Serial.print("Real altitude = ");
+  Serial.print(bmp.readAltitude(seaLevelPressure_hPa * 100));
+  Serial.println(" meters");
+    
+  Serial.println();
+  delay(10000);
+
+
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+  delay(500);
 }
 
-}
